70e7cea8f054cb7bf6e528e3bde6f9a2710d85de
Andres Carrillo <acarrillolopez98@gmail.com>
2019-08-02 11:43:40 +0200

    Added model checkpoints and feature scaling savefile.

--- src/fitness/MLP.py
+++ src/fitness/MLP.py
@@ -1,49 +1,74 @@
+'''
+Keras implementation of the Python's Genetic Algorithm approach
+on PonyGE2 framework. 
+
+This project is a Work in Progress, so right now it does not run.
+'''
+
+
 #Genetic Algorithm Library (PonyGE2)
 from fitness.base_ff_classes.base_ff import base_ff
 
-#Math, Data Science and System libraries
 import numpy  as np
 import pandas as pd
-import time , sys, os
+import pickle, time , sys, os
+from datetime import datetime
 
-# Scikit-earn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_digits
-# Keras & tensorflow
+from sklearn.preprocessing import StandardScaler
+
 import keras
 from keras.layers import Dense 
 from keras.models import Model, Sequential
 from keras import optimizers, losses
 from keras.utils import to_categorical
 from keras import backend as K
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 import tensorflow as tf
 import logging
 
+counter = 0
 
 class MLP(base_ff):
     maximise = True #we want to maximize our objective: model score.
-    
+    global counter
+
     def __init__(self):
         tf.get_logger().setLevel(logging.ERROR)
 
         super().__init__()
         #class attributes (placeholders, later as init attributes)
-        self.it         = 25
+        self.it         = 100
         self.lr         = 0.01
         self.verbose    = True
         self.optimizer  = optimizers.SGD(lr = self.lr, momentum = 0.9,nesterov = True)
 
+        scaler = StandardScaler()
+
         data = load_digits()
         X = data.data
-        X = (X - np.min(X)) / (np.max(X) - np.min(X))
         y = data.target
         
+        X, X_reserved, y, y_reserved  = train_test_split(X,y, random_state = 22,test_size = 0.25)
+        
+
         self.classes_, self.y = np.unique(y, return_inverse=True)
         self.n_classes = self.classes_.shape[0]
-        #right now I wont be using testing split, so is very small
-        self.X, self.X_test, self.y, self.y_test = train_test_split(X, y, test_size=0.1)
+
+        self.X, self.X_test, self.y, self.y_test = train_test_split(X, y, test_size=0.2)
+        self.X = scaler.fit_transform(self.X)
+        self.X_test = scaler.transform(self.X_test)
+        
+        #Keeping scaler mean and var for later use
+        (s_mean, s_var) = scaler.mean_, scaler.var_
+        self.fname = datetime.now().strftime('%H:%M:%S.csv')
+        scaling_filename = "./results/"+self.fname[:8]+"/"+"scaling_factors.txt"
+        os.makedirs(os.path.dirname(scaling_filename), exist_ok=True)
+        with open(scaling_filename, 'wb') as f:
+            pickle.dump((s_mean, s_var), f)
 
         self.X = np.array(self.X)
         self.X_test = np.array(self.X_test)
@@ -51,21 +76,37 @@ class MLP(base_ff):
         self.nn = None #Model (that will be tested, etc)
 
     def evaluate(self, ind, **kwargs):
-        #print(ind.phenotype)
-        inargs = {"xphe" : self.X.copy()}
-        exec(ind.phenotype,inargs) #self.viu, msdX, self.nn initialized here
-        #Obtain generated output from exec dictionary
-        self.nn = inargs['nn']
-        #print("Layers being used: ", self.nn.layers) #TODO: make print stm more verbose
-        #Once GE has decided model, proceed to compile, test and evaluate it.
-        self.nn.compile(loss = losses.categorical_crossentropy ,optimizer = self.optimizer, metrics=['accuracy'])
-        self.nn.fit(x = self.X, y= self.y, validation_split=0.33,verbose=0,batch_size = 32, epochs = self.it)
-                
-        #Compute target variable to minimize.
-	
-	#score would be here
-       
-  
+        global counter
+        try:
+            inargs = {"xphe" : self.X.copy()}
+            exec(ind.phenotype,inargs) 
+            #Obtain generated output from exec dictionary
+            self.nn = inargs['nn']
+            #Once GE has decided model, proceed to compile, test and evaluate it.
+            self.nn.compile(loss = losses.categorical_crossentropy ,optimizer = self.optimizer, metrics=['accuracy'])
+
+            #Best network epoch resulted on training will be stored as individual
+            fname = "./results/"+self.nsc_csv_name[:8]+"/networks/"+"Net"+str(counter)+'_fullmodel.hdf5'
+            os.makedirs(os.path.dirname(fname), exist_ok=True)
+            early_stop = EarlyStopping(monitor='val_acc',mode='max', verbose=0,patience = 10)
+            cp_save = ModelCheckpoint(fname, save_best_only=True,verbose=0, monitor='val_acc', mode='max')
+
+            self.nn.fit(x = self.X, y= self.y, validation_split=0.33,verbose=0,batch_size = 32, epochs = self.it)
+            
+            #Best network resulting from training will be used for evaluation
+            self.nn.load_weights(fname)
+            #Compute target variable to minimize.
+            model_loss, classification_ac = self.nn.evaluate(x = self.X_test, y = self.y_test, verbose = 0)
+            
+            #score would be here
+            score = classification_acc 
+
+            counter += 1
+            K.clear_session()
+        except:
+            #In case of invalid individual, set objective to minimum
+            score = 0.01
+    
         return score #this will be the target to minimize by the GE algorithm.
 
 
